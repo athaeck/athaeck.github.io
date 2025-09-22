34dd2775c3d29541a5b7775c7369bd37186de739
Nick Häcker <nick.athaeck@googlemail.com>
2025-09-22 23:01:42 +0200

    start rework

---
+++ app/components/core/layout/menu.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="nav-container">
+
+    </div>
+</template>
+
+<script>
+import Logo from "../../theme/logo.vue"
+export default {
+    name: "Menü",
+    components: {
+        Logo
+    },
+    data() {
+        return {
+            show: false,
+            changed: false
+        }
+    },
+    methods: {
+        clickedMenu() {
+            // working
+            this.show = false;
+            this.changed = false;
+        },
+        toggleButton() {
+            if (this.show == true) {
+                this.show = false;
+                this.changed = false;
+            } else {
+                this.show = true;
+                this.changed = true;
+            }
+            // this.show = !this.show
+        }
+    }
+}
+
+</script>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Menu />
+
+        <slot />
+
+        <div class="footer">
+            <!-- <Footer /> -->
+        </div>
+    </div>
+</template>>
+
+<script>
+import Menu from "../components/core/layout/menu.vue"
+
+
+export default {
+    name: "DefaultLayout",
+    components: {
+        Menu
+    }
+}
+
+</script>
